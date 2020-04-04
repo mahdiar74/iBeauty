916a8df7076dd6698fe02387825e1fb6e74516aa
mahdiar74 <mahdiarrr74@gmail.com>
2020-04-05 00:23:39 +0430

    Bootstrap4 Added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <link rel="stylesheet" href="{{ url('/css/bootstrap4.min.css') }}">
+
     @yield('links')
     @yield('meta')
     @yield('styles')
 </head>
 <body>
-    
+
     <header></header>
 
     <main>
@@ -15,6 +18,9 @@
 
     <footer></footer>
 
+
+    <script src="{{ url('/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ url('/js/bootstrap.bundle.min.js') }}"></script>
     @yield('scripts')
 </body>
 </html>
